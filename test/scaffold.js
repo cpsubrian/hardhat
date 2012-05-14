a1a2ef18bd7454f112567163b1bb6c41a5f0f1f8
Brian Link <cpsubrian@gmail.com>
2012-05-14 12:11:56 -0700

    Fixes error handling and adds tests for errors.

--- test/scaffold.js
+++ test/scaffold.js
@@ -68,4 +68,25 @@ describe('Scaffold', function() {
       assert.equal(contents, '<html><head><title>Hot Title</title></head><body>Sexy Body</body></html>');
     });
   });
+
+  describe('Error Conditions', function() {
+    it('should error on a bad input directory', function(done) {
+      dirIn = path.join(__dirname, 'fixtures/bad');
+      hardhat.scaffold(dirIn, dirOut, function(err) {
+        assert.ok(err, 'An error was not present.')
+        assert.equal(err.message.indexOf('The input directory does not exist'), 0, 'An incorrect error message was observed.')
+        done();
+      });
+    });
+
+    it('should error on a bad templating engine', function(done) {
+      dirIn = path.join(__dirname, 'fixtures/plain');
+      hardhat.scaffold(dirIn, dirOut, {engine: 'bad'}, function(err) {
+        assert.ok(err, 'An error was not present.')
+        assert.equal(err.message.indexOf('The templating engine'), 0, 'An incorrect error message was observed.')
+        done();
+      });
+    });
+  });
+
 });
